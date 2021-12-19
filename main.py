d958b632cbc908b88fa152ee322f6565f4833556
VetrovAnton228 <55150813+Anton554@users.noreply.github.com>
2021-12-19 13:54:08 +0300

    Расстановка комментариев метода train()

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from torch.nn import functional as F
 from torch.utils import data
 from torchvision import transforms
 
-trans = transforms.Compose([transforms.Resize((28,28)),
+trans = transforms.Compose([transforms.Resize((28, 28)),
                             transforms.ToTensor(),
                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                            std=[0.229, 0.224, 0.225])])
+                                                 std=[0.229, 0.224, 0.225])])
 
 train_data = torchvision.datasets.ImageFolder(root=r'./img/train', transform=trans)
 val_data = torchvision.datasets.ImageFolder(root=r'./img/val', transform=trans)
@@ -17,19 +17,19 @@ batch_size = 64
 train_data_loader = data.DataLoader(train_data, batch_size)
 val_data_loader = data.DataLoader(val_data, batch_size)
 test_data_loader = data.DataLoader(test_data, batch_size)
-# print(type(train_data_loader))
+
 
 class SimpleNet(nn.Module):
     def __init__(self):
         super(SimpleNet, self).__init__()
         # создаем 3 полносвязных слоя и
         # указываем колличество входных и выходных нейронов
-        self.fc1 = nn.Linear(28*28*3, 84)
+        self.fc1 = nn.Linear(28 * 28 * 3, 84)
         self.fc2 = nn.Linear(84, 50)
         self.fc3 = nn.Linear(50, 2)
 
     def forward(self, x):
-        x = x.reshape(-1, 28*28*3)
+        x = x.reshape(-1, 28 * 28 * 3)
         # указываем функцию активации на каждом слое
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
--- train_model.py
+++ train_model.py
@@ -10,38 +10,81 @@ import main
 
 
 def train(model, optimizer, loss_fn, train_loader, val_loader, epochs=10):
+    """
+
+    :param model: объект класса SimpleNet - модель нейронной сети
+    :param optimizer: объект оптимизатор с алгоритмом "Адам"
+    :param loss_fn: объект "функция ошибок" - подкласс CrossEntropyLoss
+    :param train_loader: набор тренировочных данных shape(64, 3, 28, 28)
+    :param val_loader: набор проверочных данных shape(64, 3, 28, 28)
+    :param epochs: кол-во эпох
+    :return:
+    """
     for epoch in range(epochs):
+        # Обнуление ТРЕНИРОВОЧНОГО и ПРОВЕРОЧНОГО счетчика ошибок
         train_loss = 0.0
         valid_loss = 0.0
+        # Включение ОБУЧАЮЩЕГО режима у модели
         model.train()
         for batch in train_loader:
+            # После каждого батча обнуляем градиент
             optimizer.zero_grad()
+            # inputs - Tensor (64, 3, 28, 38) - картинки для обучения
+            # targets - правильный ответ Tensor(64,)
             inputs, targets = batch
+            # копируем Tensor в память cpu
             inputs = inputs.to('cpu')
             target = targets.to('cpu')
-            # print(inputs.shape)
+            # Предсказываем значаение ОБУЧАЮЩЕГО картинки
             output = model(inputs)
+            # вычисление значения "функции потерь"
             loss = loss_fn(output, target)
+            # вычисление градиента "функции потерь" (вектор направление)
             loss.backward()
+            # обновление градиента "функции потерь" обратное распространение ошибки
             optimizer.step()
+            # накапливаем значение "функции потерь"
             train_loss += loss.data.item()
+        # для каждой эпохи находим среднее арифмитическое значение "функции потерь"
+        # в итоге получаем среднее значение функции ошибки ТРЕНИРОВОЧНОГО пакета
         train_loss /= len(train_loader)
+        # Включение ПРОВЕРОЧНОГО режима у модели
         model.eval()
+        # обнуление счетчиков
         num_correct = 0
         num_examples = 0
         for batch in val_loader:
+            # inputs - Tensor (64, 3, 28, 38) - картинки для ПРОВЕРКИ
+            # targets - правильный ответ Tensor(64,)
             inputs, targets = batch
+            # копируем Tensor в память cpu
             inputs = inputs.to('cpu')
-            output = model(inputs)
             targets = targets.to('cpu')
+            # Предсказываем значаение ПРОВЕРОЧНОЙ картинки
+            output = model(inputs)
+            # вычисление значения "функции потерь"
             loss = loss_fn(output, targets)
+            # накапливаем значение "функции потерь"
             valid_loss += loss.data.item()
+            """1) softmax мерному входному тензору, масштабируя их так, чтобы элементы n-мерного выходного тензора 
+            находились в диапазоне [0,1] 
+            2) max возвращает namedtuple(values, indices), где values максимальное значение каждой строки inputтензора
+             в данном измерении dim
+            3) eq вычисляет поэлементное равнство
+            4) получаем тензор tensor([ True,  True,  True,  True])"""
             correct = torch.eq(torch.max(F.softmax(output), dim=1)[1], targets).view(-1)
+            # кол-во правильных ответов
             num_correct += torch.sum(correct).item()
+            # накапливаем общее кол-во проверок
             num_examples += correct.shape[0]
+        # для каждой эпохи находим среднее арифмитическое значение "функции потерь"
+        # в итоге получаем среднее значение функции ошибки ПРОВЕРОЧНОГО пакета
         valid_loss /= len(val_loader)
 
-        print('Epoch: {}, Training Loss: {:.2f}, Validation Loss: {:.2f}, accuracy = {:.2f}'.format(epoch, train_loss, valid_loss, num_correct / num_examples))
+        print('Epoch: {}, Training Loss: {:.2f}, Validation Loss: {:.2f}, accuracy = {:.2f}'.format(epoch, train_loss,
+                                                                                                    valid_loss,
+                                                                                                    num_correct / num_examples))
+
 
 def dunw_net():
     with open('simplenet.pth', 'rb') as f:
@@ -67,11 +110,10 @@ def test(model, test_loader):
         print('Accuracy of the model based on the test set of '
               'inputs is: %d %%' % (100 * running_accuracy / total))
 
+
 if __name__ == "__main__":
     simplenet = SimpleNet()
     optimizator = optim.Adam(simplenet.parameters(), lr=0.001)
     train(simplenet, optimizator, torch.nn.CrossEntropyLoss(), main.train_data_loader, main.val_data_loader)
     with open('simplenet.pth', 'wb') as f:
         pickle.dump(simplenet, f)
-    #
-    # test(dunw_net(), main.test_data_loader)
