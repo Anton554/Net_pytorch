7584e50c41bf17ab72e3670253895cb639953023
VetrovAnton228 <55150813+Anton554@users.noreply.github.com>
2021-12-30 15:39:17 +0300

    Добавлен функция print_proc для вывода процента предсказаний

--- predict_one.py
+++ predict_one.py
@@ -3,21 +3,29 @@ import model_cnn
 import cv2 as cv
 
 
-def predict(net, img_name:str):
+def print_proc(proc):
+    for num, el in enumerate(proc, start=0):
+        print(f'{num} - ({round(el, 3)})')
+    print()
+
+
+def predict(net, img_name: str):
     np_arr = cv.imread(img_name)  # shape [28, 28, 3]
-    print(f'{np_arr.shape=}')
+    # print(f'{np_arr.shape=}')
     transform = model_cnn.get_transform()
     tr = transform(np_arr)
-    input = tr.reshape(1,3,28,28)
-    print(f'{input.shape=}')
+    input = tr.reshape(1, 3, 28, 28)
+    # print(f'{input.shape=}')
 
     pred = net(input)
-    print(f'{pred.shape=}')
+    # print(f'{pred=}')
+    proc = [tn.item() for tn in pred[0]]
     pred = pred.argmax()
-    return pred.item()
+    return pred.item(), proc
 
 
 if __name__ == '__main__':
     net = torch.load('./net/cnn_net_3ch.pth')
-    pred = predict(net, './img/td/9/num-11.jpg')
-    print(pred)
+    pred, proc = predict(net, './img/predict/7-img_33.png')
+    print_proc(proc)
+    print(f'Ваше число - {pred}')
